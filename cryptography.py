5f0a1fef353e50e39d8a3683e6e6b66fe3b6b8c1
voidhidan13 <maeva.garcia13210@gmail.com>
2026-04-21 11:50:12 +0200

    Récupération clé

---
+++ cryptography.py
@@ -0,0 +1,27 @@
+from cryptography.fernet import Fernet
+
+#Fonction chiffrer un fichier
+def chiffrer_fichier(file_name = "secret.txt"):
+    ma_cle = Fernet.generate_key()
+    print(f"La clé de chiffrement est : {ma_cle}")
+    machine_aes = Fernet(ma_cle)
+
+#On ouvre le fichier en mode Lecture d'octet via rb 
+    with open(file_name, "rb") as file_toencrypt :
+       #On Stocke les données à chiffrer dans une variable
+       données_à_chiffrer = file_toencrypt.read()
+       #On passe cette Variable dans la fontion d'Encryption
+       données_chiffrés = machine_aes.encrypt(données_à_chiffrer)
+       print(f"Le chiffrement du fichier est terminé")
+#Une fois les données chiffrés ont les stockes dans un nouveau fichier
+    with open (file_name,"wb") as fichier_verrouillé :
+        fichier_verrouillé.write(données_chiffrés)
+#On retourne la clé
+    return ma_cle
+
+if __name__ == "__main__":
+    print("Lancement du crypteur...")
+    
+    # Le chef d'orchestre lance la fonction et récupère la clé dans une boîte !
+    cle_secrete = chiffrer_fichier() 
+    
